687950b9e20963950e9624669d34eacb0eecd125
Fernando Mercês <fernando@mentebinaria.com.br>
2013-11-25 02:47:46 -0200

    code improvments on repacker.sh and malloc_s files additions

---
+++ lib/malloc_s.h
@@ -0,0 +1,6 @@
+#ifndef __MALLOC_S__
+#define __MALLOC_S__
+
+void *malloc_s(size_t);
+
+#endif
--- repacker.sh
+++ repacker.sh
@@ -5,59 +5,46 @@
 #Generate Key
 # keytool -genkey -v -keystore dexterity.keystore -alias dexterity -keyalg RSA -keysize 2048 -validity 10000
 
-if  [ $# -ne 4 ]; then
-    echo "$0 apk dex keystore alias"
-else
-    apk=$(basename $1)
-    dex=$(basename $2)
-    keystore=$(basename $3)
-    keyalias=$4
+[ $# -eq 4 ] || { echo -e "Usage:\n\t$0 apk dex keystore alias"; exit 1; }
 
-    apkdir=$(cd $(dirname $1); pwd)
-    dexdir=$(cd $(dirname $2); pwd)
-    keydir=$(cd $(dirname $3); pwd)
-   
-    echo "<repacker> --- DEX Repacker v1 ---"
-    echo "<repacker> APK: $apk"
-    echo "<repacker> DEX: $dex"
+for i in zip unzip jarsigner; do
+	which $i >/dev/null || { echo $i not found; exit 1; }
+done
 
-    name=${apk%.*}
-    
-    tmpfolder="$name-tmp"
-    target="$name-repack.apk"
+apk=$(basename $1)
+dex=$(basename $2)
+keystore=$(basename $3)
+keyalias=$4
 
-    echo "<repacker> Extracting DEX file"
+apkdir=$(cd $(dirname $1); pwd)
+dexdir=$(cd $(dirname $2); pwd)
+keydir=$(cd $(dirname $3); pwd)
 
-    if [ -d "$tmpfolder" ]; then
-	rm -rf $tmpfolder
-    fi
+echo -e \
+"<repacker> --- DEX Repacker v1 ---\n\
+<repacker> APK: $apk\n\
+<repacker> DEX: $dex"
 
-    unzip -d $tmpfolder "$apkdir/$apk"
+tmpfolder=$(mktemp -d)
+target="${apk%.*}-repack.apk"
 
-    echo "<repacker> Replacing DEX"
-    cp "$dexdir/$dex" "$tmpfolder/classes.dex"
-    
-    echo "<repacker> Removing meta info"
-    rm -rf "$tmpfolder/META-INF"
+echo "<repacker> Extracting DEX file"
+unzip -d $tmpfolder "$apkdir/$apk"
 
-    echo "<repacker> Repackaging APK"
-    cd $tmpfolder; 
-    zip -r $target ./;
-    cd ..
+echo "<repacker> Replacing DEX"
+cp "$dexdir/$dex" "$tmpfolder/classes.dex"
 
-    if [ -f "$target" ]; then
-	rm $target
-    fi
+echo "<repacker> Removing meta info"
+rm -rf "$tmpfolder/META-INF"
 
-    mv "$tmpfolder/$target" .
+echo "<repacker> Repackaging APK"
+rm -f $target
+zip -r $target $tmpfolder
 
-    echo "<repacker> Signing the APK"
-    jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore "$keydir/$keystore" $target $keyalias
-    
-    echo "<repacker> Cleaning tmp"
-    rm -rf $tmpfolder
+echo "<repacker> Signing the APK"
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore "$keydir/$keystore" $target $keyalias
 
-    echo "<repacker> Done."    
+echo "<repacker> Cleaning tmp"
+rm -rf $tmpfolder
 
-    exit 0
-fi
+echo "<repacker> Done."    
