338252834143ab3a27f5fc9112c96c58a9e70ddf
Rodrigo Chiossi <rchiossi@gmail.com>
2013-11-25 22:55:35 -0200

    addstr tweaks

--- addstr.py
+++ addstr.py
@@ -3,25 +3,26 @@
 import argparse
 
 from dx.dex import Dex
-
-from dx.hash import update_signature, update_checksum
+from dx.hash import update_signature
+from dx.hash import update_checksum
 
 def main():
-    parser = argparse.ArgumentParser(description="Parse and reconstruct dex file")
+    parser = argparse.ArgumentParser(description="Add a string to a DEX file.")
 
+    parser.add_argument('source',help='Source DEX file')
     parser.add_argument('target',help='Target DEX file')
     parser.add_argument('string',help='String to be added')
 
     args = parser.parse_args() 
 
-    dex = Dex(args.target)
+    dex = Dex(args.source)
 
     dex.add_string(args.string)
 
-    dex.save("out2.dex")
+    dex.save(args.target)
 
-    update_signature("out2.dex")
-    update_checksum("out2.dex")
+    update_signature(args.target)
+    update_checksum(args.target)
 
     print "Done"
 
